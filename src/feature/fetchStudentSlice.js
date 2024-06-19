f57ea5377c74ba46ccda97e6230b31299dadd5dc
xwiit <switluvsangels@gmail.com>
2024-06-19 16:01:02 +0100

    working on token

--- src/feature/fetchStudentSlice.js
+++ src/feature/fetchStudentSlice.js
@@ -12,6 +12,7 @@ export const fetchStudent = createAsyncThunk(
           Authorization: `Bearer ${authToken}`,
         },
       });
+      console.log(response.data.data);
       return response.data.data;
     } catch (error) {
       console.log(error);
