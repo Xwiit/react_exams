178a6100cc81721bd9158d0ea1f69a9ac2450c30
xwiit <switluvsangels@gmail.com>
2024-06-23 22:39:26 +0100

    student dashboard

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -2,15 +2,23 @@ import { useSelector } from "react-redux";
 import logo from "../images/logo.jpg";
 import { getStduent, getResult } from "../feature/service";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import ResultPreview from "../componets/ResultPreview";
 
 function StudentDashboard() {
   const student = getStduent();
   const navigate = useNavigate();
-  const result = getResult();
+  const [isResultReady, setIsResultReady] = useState(false);
 
+  // const result = getResult();
+
+  // if (result === null) {
+  //   setIsResultReady(false);
+  //   return;
+  // } else {
+  //   setIsResultReady(true);
+  // }
   // const authToken =
   //   "d3727bee94080f3fc878c1047247791e86df67c49c932d7d4b1851d07e22c4f4fd9690804377fe96663d22f59986599466950bd2739c3115e96bdd9cb801fcde070371b5dcbed867b6fb576d5bf7a9997756b60699e206ae25972ff933f17057fa8df5c0ec0676bc9722516bf0f2ef5e0ad12e72bbbecdf3cc93d799bd1ed0ad";
 
@@ -64,6 +72,7 @@ function StudentDashboard() {
         localStorage.setItem("affectiveSkills", JSON.stringify(affective));
         localStorage.setItem("session", JSON.stringify(session));
         localStorage.setItem("remarks", JSON.stringify(remarks));
+
         return {
           data1: result.data,
           data2: psychomotor.data,
@@ -75,6 +84,7 @@ function StudentDashboard() {
         // return rejectWithValue(error);
       }
     }
+
     getStdResults();
   }, [authToken, stdID, serverUrl]);
 
@@ -92,9 +102,6 @@ function StudentDashboard() {
     navigate("/");
   }
 
-  function handleLoadResult() {
-    location.reload(); // Reloads the current page
-  }
   return (
     <div
       className="bg-background w-full h-screen flex justify-center  scrollbar-thin 
@@ -102,7 +109,7 @@ scrollbar-webkit overflow-y-scroll
     "
     >
       <div className="w-[90%] sm:w-[70%] h-[55rem] rounded-lg my-[2rem] sm:mb-0 sm:rounded-none bg-gray-200">
-        <div className="bg-forecolor sm:px-[2rem] text-gray-200 w-full items-center m-4rem h-[30%] sm:h-[30%] sm:flex sm:flex-row flex-col p-2 sm:justify-between">
+        <div className="bg-forecolor sm:px-[2rem] text-gray-200 w-full items-center m-2rem h-[40%] sm:h-[30%] sm:flex sm:flex-row flex-col p-2 sm:justify-between">
           <div className="w-full flex justify-center items-center  sm:w-[20%]">
             <img
               // src={`http://localhost:1337/${student?.attributes?.passport?.data?.attributes?.url}`}
@@ -122,28 +129,32 @@ scrollbar-webkit overflow-y-scroll
             <div className="flex uppercase flex-col text-xl justify-between">
               <small>Examniation No: {student?.attributes?.admissionNo}</small>
             </div>
+            {isResultReady === false ? (
+              <div>
+                <button
+                  className="btn"
+                  onClick={() => {
+                    setIsResultReady(true);
+                  }}
+                >
+                  View Result
+                </button>
+              </div>
+            ) : null}
           </div>
 
           <div className="w-[10%]">
-            <small
-              onClick={handleResultLogout}
-              className="cursor-pointer p-2 text-background bg-gray-300 rounded"
-            >
-              Logout
-            </small>
-          </div>
-        </div>
-        {
-          <div>
-            {result ? (
-              <button className="btn" onClick={handleLoadResult}>
-                Load Result
-              </button>
-            ) : (
-              <ResultPreview />
+            {isResultReady && (
+              <small
+                onClick={handleResultLogout}
+                className="cursor-pointer p-2 text-background bg-gray-300 rounded"
+              >
+                Logout
+              </small>
             )}
           </div>
-        }
+        </div>
+        {isResultReady && <div>{<ResultPreview />}</div>}
       </div>
     </div>
   );
